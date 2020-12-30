4b63a0c0c2393bb8b561571d1fcc62012400ac38
yur <mytem20007@gmail.com>
2020-12-30 20:59:53 +0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,5 @@
+start https://nodejs.org/dist/latest-v12.x/
 npm install
-json-server --watch db.json --port 3001
+node_modules\.bin\json-server.cmd --watch db.json --port 3001
+start localhost:3001
 npm run start
